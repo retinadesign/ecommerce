23855662ce6d9381ce9fad0273bbfd721de6029c
Pixel In The Dark <vaultcg@gmail.com>
2013-11-14 10:02:08 +0900

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ecommerce::Application.config.secret_key_base = '5d083a1f29df5d987525cd79e40892fd33d8acb7476919a2499c4338f5fbb5ff8ad21fab4a2f3ed244ce4c87e85e832372d7bfb7412c1f66852b93caa9c5398c'
